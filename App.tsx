ae97878f40ac267f860f80967059ffd49798fbd8
Gilmara Pimentel <gilmarapq@hotmail.com>
2023-12-27 20:44:42 -0500

    feat: create SplashAnimated Screen

--- App.tsx
+++ App.tsx
@@ -1,12 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { SplashAnimated } from './src/screens/SplashAnimated';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SplashAnimated/>
   );
 }
 
---
+++ src/screens/SplashAnimated/index.tsx
@@ -0,0 +1,9 @@
+import { View } from 'react-native';
+import { styles } from './styles';
+
+export const SplashAnimated =()=>{
+    return(
+    <View style={styles.container}>
+
+    </View>)
+};
