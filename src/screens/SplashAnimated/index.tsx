f720d0f6bc2018cf33982af2825cc6da53ec4b82
Gilmara Pimentel <gilmarapq@hotmail.com>
2023-12-29 14:36:51 -0500

    feat: create SplashAnimated

--- src/screens/SplashAnimated/index.tsx
+++ src/screens/SplashAnimated/index.tsx
@@ -1,9 +1,21 @@
-import { View } from 'react-native';
-import { styles } from './styles';
+import { View , Image, Center, HStack} from 'native-base';
+import coffeeCup from '@src/assets/vector.png';
+import coffeeDelivery from '@src/assets/coffeeDelivery.png';
 
 export const SplashAnimated =()=>{
     return(
-    <View style={styles.container}>
+    <View 
+        flex={1} 
+        backgroundColor='product.brand_purple'
+        >
+            <Center flex={1}>
+                <HStack >
+                    <Image source={coffeeCup} alt='Coffee Cup' width='44' height='68' mr='5'/>
+                    <Image source={coffeeDelivery} alt='Coffee Cup' width='95' height='71'/>
+                </HStack>
 
-    </View>)
+            </Center>
+
+    </View>
+    )
 };
