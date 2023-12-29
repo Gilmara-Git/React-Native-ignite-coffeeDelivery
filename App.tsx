1d6bc9389e2a13e3f2a7890121b6f989d491905e
Gilmara Pimentel <gilmarapq@hotmail.com>
2023-12-29 13:00:36 -0500

    feat: setup the status bar and initial screen

--- App.tsx
+++ App.tsx
@@ -1,13 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { SplashAnimated } from './src/screens/SplashAnimated';
-import { NativeBaseProvider , Box } from 'native-base';
+import { StyleSheet, ActivityIndicator, StatusBar } from 'react-native';
+import { SplashAnimated } from '@screens/SplashAnimated';
+import { NativeBaseProvider , Box, Text, View } from 'native-base';
+import { Baloo2_700Bold, useFonts } from '@expo-google-fonts/baloo-2';
+import { Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { THEME } from './src/themes';
 
+
 export default function App() {
+  
+  const [ fontsLoaded ] = useFonts({
+    Baloo2_700Bold,
+    Roboto_700Bold,
+    Roboto_400Regular
+  });
+
+  if(!fontsLoaded){
+    return <ActivityIndicator />
+  };
+
+
   return (
   <NativeBaseProvider theme={THEME}>
-    <Box>Hello Coffee Delivery people</Box>
+    <StatusBar 
+      barStyle={'dark-content'} 
+      backgroundColor='transparent'
+      translucent/>
+      
+    <View style={styles.container}>
+      <View style={{alignItems: 'center',
+    justifyContent: 'center',}}>
+
+      <Text style={{
+        fontFamily: THEME.fonts.baloo2_bold, 
+        fontSize: THEME.fontSizes.title_Sm,
+        backgroundColor: THEME.colors.white,
+        paddingTop: 6
+        
+      }}>
+        Hello Coffee Delivery people
+      </Text>
+          </View>
+    </View>
   </NativeBaseProvider>
   );
 }
@@ -15,7 +48,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: THEME.colors.product['purple-dark'],
     alignItems: 'center',
     justifyContent: 'center',
   },
