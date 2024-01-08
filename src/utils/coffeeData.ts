ea1f638fa9d869f0ed404ce805fd13090922ace3
Gilmara Pimentel <gilmarapq@hotmail.com>
2024-01-08 08:40:30 -0500

    add: add coffeeData file

---
+++ src/utils/coffeeData.ts
@@ -0,0 +1,66 @@
+import traditionalEspresso from '@src/assets/traditionalEspresso.png';
+import americanEspresso from '@src/assets/americanEspresso.png';
+import creamedEspresso from '@src/assets/creamedEspresso.png';
+import latte from '@src/assets/latte.png';
+import  coldEspresso from '@src/assets/coldEspresso.png';
+import cappuccino from '@src/assets/cappuccino.png';
+import mocaccino from '@src/assets/mocaccino.png';
+import hotChocolate from '@src/assets/hotChocolate.png';
+import cubano from '@src/assets/cubano.png';
+import hawaiian from '@src/assets/hawaiian.png';
+import arabic from '@src/assets/arabic.png';
+import irish from '@src/assets/irish.png';
+
+
+export const sectionList_DATA = [
+    {
+    category: "Traditional",
+    data: [
+        {label: 'Traditional', imgSrc: traditionalEspresso, title: "Traditional Espresso", description:'Traditional coffee made with hot water and ground coffee', price: '9.90'},
+        {label: 'Traditional', imgSrc: americanEspresso, title: "American Espresso", description:'Diluted expresso, light than the Traditional Espresso', price: '8.90'},
+        {label: 'Traditional', imgSrc: creamedEspresso, title: "Creamed Espresso", description:'Foamed Expresso coffee', price: '7.90'},
+        {label: 'Traditional', imgSrc: latte, title: "Latte", description:'Express Coffee with double shot of milk and foam', price: '9.00'},
+        {label: 'Traditional', imgSrc: coldEspresso, title: "Cold Espresso", description:'Iced Expresso coffee', price: '6.90'},
+    ]
+},
+
+{
+    category: "Sweet",
+    data: [
+        {label: 'Sweet', imgSrc: cappuccino, title: "Cappuccino", description:'Made with coffee, milk, foam and cinnamon', price: '10.70'},
+        {label: 'Sweet', imgSrc: mocaccino, title: "Mocaccino", description:'Expresso coffee with chocalate shot, milk and foam', price: '10.00'},
+        {label: 'Sweet', imgSrc: hotChocolate, title: "Hot Chocolate", description:'Diluted chocolate diluted in hot milk and coffee', price: '9.10'},
+    ]
+},
+{
+    category: "Special",
+    data: [
+        {label: 'Special', imgSrc: cubano, title: "Cubano", description:'Cold Expresso coffee with a shot of rum, cream milk and mint', price: '11.50'},
+        {label: 'Special', imgSrc: hawaiian, title: "Hawaiian", description:'Coffee mixed with coconut milk', price: '9,20'},
+        {label: 'Special', imgSrc: arabic, title: "Arabic", description:'Arabic coffee drink and spices', price: '8.85'},
+        {label: 'Special', imgSrc: irish, title: "Irish", description:'Coffee with a shot of Irish whiskey, sugar and whipped cream', price: '9.29'},
+       
+    ]
+}
+
+
+];
+
+
+
+export const flatList_DATA = [
+     
+        {label: 'Traditional', imgSrc: traditionalEspresso, title: "Traditional Espresso", description:'Traditional coffee made with hot water and ground coffee', price: '9.90'},
+        {label: 'Traditional', imgSrc: americanEspresso, title: "American Espresso", description:'Diluted expresso, light than the Traditional Espresso', price: '8.90'},
+        {label: 'Traditional', imgSrc: creamedEspresso, title: "Creamed Espresso", description:'Foamed Expresso coffee', price: '7.90'},
+        {label: 'Traditional', imgSrc: latte, title: "Latte", description:'Express Coffee with double shot of milk and foam', price: '9.00'},
+        {label: 'Traditional', imgSrc: coldEspresso, title: "Cold Espresso", description:'Iced Expresso coffee', price: '6.90'},
+        {label: 'Sweet', imgSrc: cappuccino, title: "Cappuccino", description:'Made with coffee, milk, foam and cinnamon', price: '10.70'},
+        {label: 'Sweet', imgSrc: mocaccino, title: "Mocaccino", description:'Expresso coffee with chocalate shot, milk and foam', price: '10.00'},
+        {label: 'Sweet', imgSrc: hotChocolate, title: "Hot Chocolate", description:'Diluted chocolate diluted in hot milk and coffee', price: '9.10'},
+        {label: 'Special', imgSrc: cubano, title: "Cubano", description:'Cold Expresso coffee with a shot of rum, cream milk and mint', price: '11.50'},
+        {label: 'Special', imgSrc: hawaiian, title: "Hawaiian", description:'Coffee mixed with coconut milk', price: '9,20'},
+        {label: 'Special', imgSrc: arabic, title: "Arabic", description:'Arabic coffee drink and spices', price: '8.85'},
+        {label: 'Special', imgSrc: irish, title: "Irish", description:'Coffee with a shot of Irish whiskey, sugar and whipped cream', price: '9.29'},   
+
+];
